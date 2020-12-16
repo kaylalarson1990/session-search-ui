c02aee8df0e7f2bd048f064a6f45712053e26577
Kayla Larson <kayla@radialdevgroup.com>
2020-12-16 15:42:11 -0700

    handle change on screen height input

--- src/Components/ScreenSize.js
+++ src/Components/ScreenSize.js
@@ -1,17 +1,31 @@
 import React from 'react';
 
-function ScreenSize() {
-  return (
-    <>
-      <div className="screen-size-select">is</div>
-      <select name="search-field" className="search-select">
-        <option>between</option>
-      </select>
-      <input placeholder="0"></input>
-      <div className="screen-size-select">and</div>
-      <input placeholder="0"></input>
-    </>
-  )
-}
+export default class ScreenSize extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstValue: '',
+      secondValue: ''
+    }
+  }
 
-export default ScreenSize;
+  handleChange = (e) => {
+    const value = e.target.value
+    this.setState({ [e.target.name]: value });
+  }
+
+  render() {
+    const { firstValue, secondValue } = this.state;
+    return (
+      <>
+        <div className="screen-size-select">is</div>
+        <select name="search-field" className="search-select">
+          <option>between</option>
+        </select>
+        <input placeholder="0" name="firstValue" value={firstValue} onChange={this.handleChange}></input>
+        <div className="screen-size-select">and</div>
+        <input placeholder="0" name="secondValue" value={secondValue} onChange={this.handleChange}></input>
+      </>
+    )
+  }
+}
